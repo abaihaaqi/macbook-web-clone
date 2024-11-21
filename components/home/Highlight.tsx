fce273c528b8496562175da30d84be090728f518
Nizar Baihaqi <nizar.albaihaqi252@gmail.com>
2024-11-21 10:58:27 +0700

    Implementasi slideshow pada highlight

--- components/home/Highlight.tsx
+++ components/home/Highlight.tsx
@@ -5,9 +5,57 @@ import hl1 from "@/public/highlight/hl-1.jpg";
 import hl2 from "@/public/highlight/hl-2.jpg";
 import hl3 from "@/public/highlight/hl-3.jpg";
 import hl4 from "@/public/highlight/hl-4.jpg";
-import { IconPlayerPlayFilled } from "@tabler/icons-react";
+import {
+  IconPlayerPauseFilled,
+  IconPlayerPlayFilled,
+} from "@tabler/icons-react";
+import { Slide, SlideProps } from "react-slideshow-image";
+import { useEffect, useState } from "react";
 
 function Highlight() {
+  const Indicators = () => {
+    return (
+      <button className="hover:cursor-pointer p-2">
+        <div className="dot bg-gray-500 h-2 rounded-full transition-all" />
+      </button>
+    );
+  };
+
+  const defaultProp: SlideProps = {
+    children: undefined,
+    arrows: false,
+    autoplay: true,
+    infinite: false,
+    indicators: Indicators,
+    transitionDuration: 800,
+    easing: "ease-out",
+  };
+
+  const [prop, setProp] = useState(defaultProp);
+
+  useEffect(() => {
+    // Cari elemen dengan class .indicators
+    const containerElement = document.querySelector(
+      ".highlight .indicatorContainer"
+    );
+    if (containerElement) {
+      return;
+    }
+
+    // Cari elemen dengan class .indicators
+    const indicatorsElement = document.querySelector(".highlight .indicators");
+
+    if (indicatorsElement) {
+      // Buat elemen container baru
+      const container = document.createElement("div");
+      container.className = "indicatorContainer";
+
+      // Masukkan container sebagai parent dari .indicators
+      indicatorsElement.parentNode?.insertBefore(container, indicatorsElement);
+      container.appendChild(indicatorsElement);
+    }
+  }, []);
+
   return (
     <section className="pt-14">
       <div className="px-8 mb-8">
@@ -16,14 +64,14 @@ function Highlight() {
       </div>
 
       {/* Slideshow */}
-      <div className="relative w-svw h-[60vh] overflow-hidden">
-        <div className="absolute flex w-max">
+      <div className="highlight relative">
+        <Slide {...prop}>
           <div className="relative h-[60vh] w-svw overflow-hidden">
             <Image
               src={hl1}
               fill
               alt="Highlight Image"
-              className="object-cover scale-110"
+              className="object-cover scale"
             />
             <p className="absolute top-8 inset-x-8 text-white text-xl font-semibold">
               Chip paling canggih yang pernah ada untuk komputer pribadi.
@@ -63,28 +111,19 @@ function Highlight() {
               Laptop pro tiasa tandingan. kini dalam warna baru: Hitam kosmik.
             </p>
           </div>
-        </div>
-      </div>
-
-      {/* Slide Control */}
-      <div className="flex justify-center gap-5 my-5">
-        <div className="bg-gray-200 rounded-full flex items-center">
-          <button className="py-6 pl-6 pr-2">
-            <div className="bg-gray-500 w-8 h-2 rounded-full"></div>
-          </button>
-          <button className="py-6 px-2">
-            <div className="bg-gray-500 w-2 h-2 rounded-full"></div>
-          </button>
-          <button className="py-6 px-2">
-            <div className="bg-gray-500 w-2 h-2 rounded-full"></div>
-          </button>
-          <button className="py-6 pl-2 pr-6">
-            <div className="bg-gray-500 w-2 h-2 rounded-full"></div>
+        </Slide>
+        <div className="absolute -bottom-20 left-1/2 -translate-x-1/2 flex ml-[80px]">
+          <button
+            onClick={() => setProp({ ...prop, autoplay: !prop.autoplay })}
+            className="bg-gray-200 rounded-full p-3"
+          >
+            {prop.autoplay ? (
+              <IconPlayerPauseFilled size={32} />
+            ) : (
+              <IconPlayerPlayFilled size={32} />
+            )}
           </button>
         </div>
-        <button className="bg-gray-200 rounded-full p-3">
-          <IconPlayerPlayFilled size={30} />
-        </button>
       </div>
     </section>
   );
